273706b928e7050c90130d14558ac81c3cd062ce
Max Vitek <maxvitek@gmail.com>
2014-03-19 00:33:44 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='intradata',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/maxvitek/intradata',
+    license='',
+    author='maxvitek',
+    author_email='',
+    description=''
+)
