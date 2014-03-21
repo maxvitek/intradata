b97a75822b51de81fc0c0c53590375405c99646e
Max Vitek <maxvitek@gmail.com>
2014-03-20 22:16:50 -0500

    renamed package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='intradata',
-    version='0.0.1',
+    version='0.0.2',
     packages=[''],
     url='https://github.com/maxvitek/intradata',
     license='',
