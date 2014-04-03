b23f534421510a9f2d30eae763005cebee3f20e6
Max Vitek <maxvitek@gmail.com>
2014-04-03 15:19:02 -0500

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='intradata',
-    version='0.0.2',
+    version='0.1.0',
     packages=[''],
     url='https://github.com/maxvitek/intradata',
     license='',
